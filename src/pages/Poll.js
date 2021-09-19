4c1139f962299c2cf6f18613767177427992c53d
Confidence Okoghenun <cokoghenun@gmail.com>
2021-09-19 16:05:05 +0100

    feat: Adds result in polls page

--- src/pages/Poll.js
+++ src/pages/Poll.js
@@ -3,48 +3,90 @@ import Button from '../components/Button';
 import Card from '../components/Card';
 
 const Poll = () => {
-  const [options, setOptions] = useState('');
+  const [selectedOption, setSelectedOption] = useState('huey');
+  const [showResults, setShowResults] = useState(false);
 
   const handleOptionChange = (e) => {
     // e.preventDefault();
-    console.log(options);
-    setOptions(e.target.value);
+    setSelectedOption(e.target.value);
   };
+
+  const handleSubmit = () => {
+    setShowResults(true);
+  };
+  
   return (
     <div className='poll'>
       <Card title='Tyler Asks:'>
-        <h2 className='poll-title'>Would You Rather...</h2>
+        {!showResults ? (
+          <div className='poll-question'>
+            <h2 className='poll-title'>Would You Rather...</h2>
 
-        <div className='poll-options'>
-          <div>
-            <input
-              type='radio'
-              id='huey'
-              name='options'
-              value='huey'
-              checked
-              onChange={handleOptionChange}
-            />
-            <label htmlFor='huey'>Huey</label>
-          </div>
+            <div className='poll-options'>
+              <div>
+                <label>
+                  <input
+                    type='radio'
+                    name='options'
+                    value='huey'
+                    checked
+                    onChange={handleOptionChange}
+                  />
+                  Huey
+                </label>
+              </div>
 
-          <div>
-            <input
-              type='radio'
-              id='dewey'
-              name='options'
-              value='dewey'
-              onChange={handleOptionChange}
-            />
-            <label htmlFor='dewey'>Dewey</label>
+              <div>
+                <label>
+                  <input
+                    type='radio'
+                    name='options'
+                    value='dewey'
+                    onChange={handleOptionChange}
+                  />
+                  Dewey
+                </label>
+              </div>
+            </div>
+            <Button text='Submit' onClick={handleSubmit} />
           </div>
-        </div>
-        <Button
-          text='Submit'
-          onClick={() => {
-            console.log('hello');
-          }}
-        />
+        ) : (
+          <div className='poll-results'>
+            <h2 className='poll-title'>Results</h2>
+
+            <div className='poll-options'>
+              <div>
+                <label>
+                  <input
+                    type='radio'
+                    name='options'
+                    value='huey'
+                    onChange={(e) => {
+                      e.preventDefault();
+                    }}
+                    checked={selectedOption === 'huey'}
+                  />
+                  Would You Rather: Huey (3/5)
+                </label>
+              </div>
+
+              <div>
+                <label>
+                  <input
+                    type='radio'
+                    name='options'
+                    value='dewey'
+                    onChange={(e) => {
+                      e.preventDefault();
+                    }}
+                    checked={selectedOption === 'dewey'}
+                  />
+                  Would You Rather: Dewey (2/5)
+                </label>
+              </div>
+            </div>
+          </div>
+        )}
       </Card>
     </div>
   );
