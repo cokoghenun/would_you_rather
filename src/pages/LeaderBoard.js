b53b93c9df073e11e1268722f38644d94af81833
Confidence Okoghenun <cokoghenun@gmail.com>
2021-09-20 15:40:14 +0100

    feat: Adds logic for leaderboards

--- src/pages/LeaderBoard.js
+++ src/pages/LeaderBoard.js
@@ -1,30 +1,31 @@
+import { useSelector } from 'react-redux';
 import Card from '../components/Card';
 
 const LeaderBoard = () => {
+  const users = useSelector((state) =>
+    state.users
+      .map((u) => ({
+        id: u.id,
+        name: u.name,
+        questions: u.questions.length,
+        answers: Object.keys(u.answers).length,
+        score: u.questions.length + Object.keys(u.answers).length,
+      }))
+      .sort((a, b) => b.score - a.score)
+  );
+
   return (
     <div className='leaderboard'>
       <ul className='leaderboard-list'>
-        <li>
-          <Card title='Sarah Paul'>
-            <p>Answered questions: 4</p>
-            <p>Created Questions: 5</p>
-            <p>Score: 10</p>
-          </Card>
-        </li>
-        <li>
-          <Card title='Sarah Paul'>
-            <p>Answered questions: 4</p>
-            <p>Created Questions: 5</p>
-            <p>Score: 10</p>
-          </Card>
-        </li>
-        <li>
-          <Card title='Sarah Paul'>
-            <p>Answered questions: 4</p>
-            <p>Created Questions: 5</p>
-            <p>Score: 10</p>
-          </Card>
-        </li>
+        {users.map((u) => (
+          <li>
+            <Card title={`${u.name}`}>
+              <p>Answered questions: {u.questions}</p>
+              <p>Created Questions: {u.questions}</p>
+              <p>Score: {u.score}</p>
+            </Card>
+          </li>
+        ))}
       </ul>
     </div>
   );
